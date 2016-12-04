fd2613ab70a2d98e0eee6175f6a0bd0725437492
Soufian <soufiazzouz@laposte.net>
2016-12-04 20:46:02 +0100

    KONAMI CODE

---
+++ Assets/Scripts/Soufian/KonamiS_.js
@@ -0,0 +1,47 @@
+﻿private var konamiCode = ["UpArrow", "UpArrow", "DownArrow", "DownArrow", "LeftArrow", "RightArrow", "LeftArrow", "RightArrow", "B", "A", "Return"];
+private var currentPos : int = 0;
+private var inKonami : boolean = false;
+ 
+function OnGUI () {
+    var e : Event = Event.current;
+    if (e.isKey && Input.anyKeyDown && !inKonami && e.keyCode.ToString()!="None") {
+        konamiFunction (e.keyCode);
+    }
+}
+ 
+function Update () 
+{
+    if (inKonami) 
+    {
+        if(!GetComponent("CameraFilterPack_FX_Glitch1").enabled)
+        {
+            GetComponent("CameraFilterPack_FX_Glitch1").enabled = true;
+        }
+    }
+    else
+    {
+        if(GetComponent("CameraFilterPack_FX_Glitch1").enabled)
+        {
+            GetComponent("CameraFilterPack_FX_Glitch1").enabled = false;
+        }
+    }
+}
+ 
+function konamiFunction (incomingKey) {
+     
+    var incomingKeyString = incomingKey.ToString();
+    if(incomingKeyString==konamiCode[currentPos]) {
+        print("Unlocked part "+(currentPos+1)+"/"+konamiCode.length+" with "+incomingKeyString);
+        currentPos++;
+         
+        if((currentPos+1)>konamiCode.length){
+            print("You master Konami.");
+            inKonami=true;
+            currentPos=0;
+        }
+    } else {
+        print("You fail Konami at position "+(currentPos+1)+", find the ninja in you.");
+        currentPos=0;
+    }
+     
+}
--- Assets/Scripts/Soufian/S_Konami.cs
+++ Assets/Scripts/Soufian/S_Konami.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class S_Konami : MonoBehaviour {
+public class S_Konami : MonoBehaviour
+{
+    /*private string[] konamiCode = ["UpArrow", "UpArrow", "DownArrow", "DownArrow", "LeftArrow", "RightArrow", "LeftArrow", "RightArrow", "B", "A", "Return"];
+    private int currentPos = 0;
+    private bool inKonami = false;
 
 	// Use this for initialization
 	void Start () {
@@ -9,7 +13,53 @@ public class S_Konami : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update ()
+    {
+        if (inKonami)
+        {
+            if (!GetComponent<CameraFilterPack_FX_Glitch1> ().enabled)
+            {
+                GetComponent<CameraFilterPack_FX_Glitch1> ().enabled = true;
+            }
+            else
+            {
+                GetComponent<CameraFilterPack_FX_Glitch1> ().enabled = false;
+            }
+        }
+    }
+
+    private void OnGUI()
+    {
+        Event e = Event.current;
+        if (e.isKey && Input.anyKeyDown && !inKonami && e.keyCode.ToString() != "None")
+        {
+            konamiFunction(e.keyCode);
+        }
+    }
+
+    void konamiFunction(string incomingKey)
+    {
+
+        string incomingKeyString = incomingKey.ToString();
+        if (incomingKeyString == konamiCode[currentPos])
+        {
+            print("Unlocked part " + (currentPos + 1) + "/" + konamiCode.Length + " with " + incomingKeyString);
+            currentPos++;
+
+            if ((currentPos + 1) > konamiCode.Length)
+            {
+                print("You master Konami.");
+                inKonami = true;
+                currentPos = 0;
+            }
+        }
+        else
+        {
+            print("You fail Konami at position " + (currentPos + 1) + ", find the ninja in you.");
+            currentPos = 0;
+        }
+
+    }
+    */
+
 }
